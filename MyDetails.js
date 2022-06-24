f85657f9caf6bfe0db490319460e271aab320b80
Claudius <claudiusluv50@gmail.com>
2022-06-24 16:53:13 +0100

    changes

---
+++ MyDetails.js
@@ -0,0 +1,8 @@
+let name, height, country;
+name = "Claudius"; //name
+height = "6 feet"; //height
+country = "Nigeria"; //Country
+
+console.log("My name is " + name + ",");
+console.log("I am  " + height + " tall" + ",");
+console.log("I am from " + country + ".");
